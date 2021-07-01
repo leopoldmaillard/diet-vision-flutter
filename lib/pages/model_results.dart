e472f22a6f3ccc6a103e2763ddfcdf468f683406
Léopold Maillard <leopoldmaillard@mbp-de-leopold.home>
2021-07-01 14:23:45 +0200

    segment camera pic #4

--- lib/pages/model_results.dart
+++ lib/pages/model_results.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'package:tflite/tflite.dart';
 
 class DisplayPictureScreen extends StatelessWidget {
   final String imagePath;
@@ -10,10 +11,112 @@ class DisplayPictureScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Display the Picture')),
+      appBar: AppBar(title: const Text('Your Meal')),
       // The image is stored as a file on the device. Use the `Image.file`
       // constructor with the given path to display the image.
-      body: AspectRatio(aspectRatio: 1, child: Image.file(File(imagePath))),
+      body: Segmentation(imagePath: this.imagePath),
+      //AspectRatio(aspectRatio: 1, child: Image.file(File(imagePath))),
+    );
+  }
+}
+
+class Segmentation extends StatefulWidget {
+  final String imagePath;
+  Segmentation({required this.imagePath});
+
+  @override
+  _SegmentationState createState() => _SegmentationState();
+}
+
+class _SegmentationState extends State<Segmentation> {
+  static List<int> pascalVOCLabelColors = [
+    Color.fromARGB(255, 0, 0, 0).value, // background
+    Color.fromARGB(255, 128, 0, 0).value, // leafy_greens
+    Color.fromARGB(255, 0, 128, 0).value, // stem_vegetables
+    Color.fromARGB(255, 128, 128, 0).value, // non-starchy_roots
+    Color.fromARGB(255, 0, 0, 128).value, // vegetables | other
+    Color.fromARGB(255, 128, 0, 128).value, // fruits
+    Color.fromARGB(255, 0, 128, 128).value, // protein | meat
+    Color.fromARGB(255, 128, 128, 128).value, // protein | poultry
+    Color.fromARGB(255, 64, 0, 0).value, // protein | seafood
+    Color.fromARGB(255, 192, 0, 0).value, // protein | eggs
+    Color.fromARGB(255, 64, 128, 0).value, // protein | beans/nuts
+    Color.fromARGB(255, 192, 128, 0).value, // starches/grains | baked_goods
+    Color.fromARGB(255, 64, 0, 128)
+        .value, // starches/grains | rice/grains/cereals
+    Color.fromARGB(255, 192, 0, 128).value, // starches/grains | noodles/pasta
+    Color.fromARGB(255, 255, 64, 64)
+        .value, // starches/grains | starchy_vegetables
+    Color.fromARGB(255, 192, 128, 128).value, // starches/grains | other
+    Color.fromARGB(255, 0, 64, 0).value, // soups/stews
+    Color.fromARGB(255, 128, 64, 0).value, // herbs/spices
+    Color.fromARGB(255, 0, 192, 0).value, // dairy
+    Color.fromARGB(255, 128, 192, 0).value, // snacks
+    Color.fromARGB(255, 0, 64, 128).value, // sweets/desserts
+    Color.fromARGB(255, 128, 64, 64).value, // beverages
+    Color.fromARGB(255, 64, 64, 128).value, // fats/oils/sauces
+    Color.fromARGB(255, 64, 64, 64).value, // food_containers
+    Color.fromARGB(255, 192, 192, 192).value, // dining_tools
+    Color.fromARGB(255, 192, 64, 64).value, // other_food
+  ];
+
+  bool _loading = true;
+  var _output;
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel().then((value) {
+      setState(() {});
+    });
+    segmentImage(widget.imagePath);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    Tflite.close();
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: 'assets/segmenter.tflite', labels: 'assets/labels.txt');
+  }
+
+  segmentImage(String imagePath) async {
+    var output = await Tflite.runSegmentationOnImage(
+      path: imagePath,
+      imageMean: 0.0,
+      imageStd: 255.0,
+      labelColors: pascalVOCLabelColors,
+    );
+    setState(() {
+      _output = output;
+      _loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size.width;
+
+    return Container(
+      child: _loading == true
+          ? null
+          : Container(
+              height: size,
+              width: size,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: MemoryImage(_output),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Opacity(
+                opacity: 0.3,
+                child: Image.file(File(widget.imagePath)),
+              ),
+            ),
     );
   }
 }
