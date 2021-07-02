9929330b678b7e5978d5adf23df59d1a2eb1d7b8
JessieRamaux <jessica.ramaux13@gmail.com>
2021-07-02 12:23:44 +0200

    #5 aske name/first name firestore

---
+++ lib/src/screen/AddUser.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+// Import the firebase_core and cloud_firestore plugin
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AddUser extends StatelessWidget {
+  final String FirstName;
+  final String Name;
+  final int age;
+
+  AddUser(this.FirstName, this.Name, this.age);
+
+  @override
+  Widget build(BuildContext context) {
+    // Create a CollectionReference called users that references the firestore collection
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    Future<void> addUser() {
+      // Call the user's CollectionReference to add a new user
+      return users
+          .add({
+            'user ID': users,
+            'first name': FirstName, // John
+            'name': Name, // Smith
+            'age': age // 42
+          })
+          .then((value) => print("User Added"))
+          .catchError((error) => print("Failed to add user: $error"));
+    }
+
+    return TextButton(
+      onPressed: addUser,
+      child: Text(
+        "Add User",
+      ),
+    );
+  }
+}
--- lib/src/screen/signup.dart
+++ lib/src/screen/signup.dart
@@ -1,30 +1,27 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:transfer_learning_fruit_veggies/src/screen/reset.dart';
-import 'package:transfer_learning_fruit_veggies/src/screen/signup.dart';
-import 'package:transfer_learning_fruit_veggies/src/screen/verify.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:transfer_learning_fruit_veggies/src/screen/AddUser.dart';
 import 'package:transfer_learning_fruit_veggies/src/screen/fhome.dart';
-import 'verify.dart';
-
 import 'package:fluttertoast/fluttertoast.dart';
 
-// This file is the design/screen part of the authentification part in an app
-
-class LoginScreen extends StatefulWidget {
+class SignUpScreen extends StatefulWidget {
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  _SignUpScreenState createState() => _SignUpScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
-  // initialize mail+pwd = avoid null error
-  String _email = '', _password = '';
+class _SignUpScreenState extends State<SignUpScreen> {
+  // instance Firestore (to initiate FlutterFire)
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  int _age = 0;
+  String _email = '', _password = '', _FirstName = '', _Name = '';
   final auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Log in'),
+          title: Text('Sign up'),
           backgroundColor: Colors.purple,
         ),
         body: Column(
@@ -55,47 +52,53 @@ class _LoginScreenState extends State<LoginScreen> {
                 },
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(hintText: 'First Name'),
+                  onChanged: (value) {
+                    setState(() {
+                      _FirstName = value.trim();
+                    });
+                  }),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(hintText: 'Name'),
+                  onChanged: (value) {
+                    setState(() {
+                      _Name = value.trim();
+                    });
+                  }),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(hintText: 'Age'),
+                onChanged: (value) {
+                  setState(() {
+                    _age = int.parse(value.trim());
+                  });
+                },
+              ),
+            ),
+
             // add button to sign in or sign up
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 RaisedButton(
                     color: Theme.of(context).accentColor,
-                    child: Text('Signin'),
-                    onPressed: () => _signin(_email, _password)),
-                RaisedButton(
-                    color: Theme.of(context).accentColor,
                     child: Text('Signup'),
-                    onPressed: () => Navigator.of(context).push(
-                        MaterialPageRoute(
-                            builder: (context) => SignUpScreen())))
-                //_signup(_email, _password)),
+                    onPressed: () => _signup(_email, _password)),
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                    child: Text('Forgot Password ?'),
-                    onPressed: () => Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => ResetScreen())))
-              ],
-            )
           ],
         ));
-  }
-
-  _signin(String _email, String _password) async {
-    try {
-      await auth.signInWithEmailAndPassword(email: _email, password: _password);
-
-      //Success
-      Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (context) => FHomeScreen()));
-    } on FirebaseAuthException catch (error) {
-      // print(error.message);
-      Fluttertoast.showToast(msg: error.message, gravity: ToastGravity.TOP);
-    }
   }
 
   _signup(String _email, String _password) async {
@@ -104,6 +107,7 @@ class _LoginScreenState extends State<LoginScreen> {
           email: _email, password: _password);
 
       //Success
+      AddUser(_FirstName, _Name, _age);
       Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (context) => FHomeScreen()));
     } on FirebaseAuthException catch (error) {
