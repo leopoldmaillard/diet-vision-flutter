ee9fd440c7609c5ba0e653ca45d4797bf34cb586
Michael <misoko.hofmann@gmail.com>
2021-07-30 12:24:56 +0200

    #31 historyMeal page cleaned

--- lib/pages/HistoryMeal.dart
+++ lib/pages/HistoryMeal.dart
@@ -5,7 +5,6 @@ import 'package:transfer_learning_fruit_veggies/services/local_storage_service.d
 import 'package:transfer_learning_fruit_veggies/bloc/food_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:transfer_learning_fruit_veggies/model/food.dart';
-import 'package:transfer_learning_fruit_veggies/pages/page2.dart';
 import 'package:transfer_learning_fruit_veggies/events/delete_food.dart';
 
 class HistoryMeal extends StatefulWidget {
@@ -26,7 +25,6 @@ class _HistoryMealState extends State<HistoryMeal> {
   void retrieveDatabase() {
     DatabaseProvider.db.getFoods().then(
       (foodList) {
-        print(foodList);
         BlocProvider.of<FoodBloc>(context).add(
           SetFoods(foodList),
         );
@@ -34,97 +32,113 @@ class _HistoryMealState extends State<HistoryMeal> {
     );
   }
 
-  showFoodDialog(BuildContext context, Food food, int index) {
-    // alert dialog come with showdialog (need context and builder)
+  /* **************************************************************************/
+  /* *****************************  PopUp Screen  *****************************/
+  /* **************************************************************************/
+
+  //show the pop up by calling displayPopup
+  dynamic showFoodDialog(BuildContext context, Food food, int index) {
     showDialog(
+      // alert dialog comes with showdialog (need context and builder)
       context: context,
-      barrierDismissible: false, //whether user can tap outside the alerdialog
+      barrierDismissible: false, //whether user can tap outside the aler dialog
       //Alert dialog are basically pop-ups with content and buttons
-      builder: (context) => AlertDialog(
-        title: Text(food.name),
-        content:
-            Text("ID ${food.id}"), //main content, image easily integratable
-        actions: <Widget>[
-          // TextButton(
-          //   onPressed: () => Navigator.pushReplacement(
-          //     context,
-          //     MaterialPageRoute(
-          //       builder: (context) => Page2(),
-          //     ),
-          //   ),
-          //   child: Text("Update"),
-          // ),
+      builder: (context) => displayPopup(food, index),
+    );
+  }
 
-          ///after we delete the food from ouf database, then we delete the
-          ///food from our foodbloc (and the dialog)
-          TextButton(
-            onPressed: () => DatabaseProvider.db.delete(food.id).then(
-              (_) {
-                BlocProvider.of<FoodBloc>(context).add(
-                  DeleteFood(index),
-                );
-                Navigator.pop(context);
-              },
-            ),
-            child: Text("Delete"),
-          ),
+  // BOUTON A FINIR : UPDATE
 
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text("Cancel"),
-          ),
-        ],
-        elevation: 24.0,
+  // TextButton(
+  //   onPressed: () => Navigator.pushReplacement(
+  //     context,
+  //     MaterialPageRoute(
+  //       builder: (context) => Page2(),
+  //     ),
+  //   ),
+  //   child: Text("Update"),
+  // ),
+
+  //Display a popup on which we can do actions on these items (update, delete..)
+  AlertDialog displayPopup(Food food, int index) {
+    return AlertDialog(
+      title: Text(food.name),
+      content: Text("ID ${food.id}"), //main content, image easily integratable
+      elevation: 24.0,
+      actions: <Widget>[
+        displayDeleteButton(food, index),
+        displayCancelButton(),
+      ],
+    );
+  }
+
+  void deleteItem(Food food, int index) {
+    DatabaseProvider.db.delete(food.id).then(
+      (_) {
+        BlocProvider.of<FoodBloc>(context).add(
+          DeleteFood(index),
+        );
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  Widget displayDeleteButton(Food food, int index) {
+    return TextButton(
+      onPressed: () => deleteItem(food, index),
+      child: Text("Delete"),
+    );
+  }
+
+  Widget displayCancelButton() {
+    return TextButton(
+      onPressed: () => Navigator.pop(context),
+      child: Text("Cancel"),
+    );
+  }
+
+  /* **************************************************************************/
+  /* ****************************  General Design  ****************************/
+  /* **************************************************************************/
+
+  BlocConsumer<FoodBloc, List<Food>> getBlocConsummer() {
+    return BlocConsumer<FoodBloc, List<Food>>(
+      builder: (context, foodList) {
+        return ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return designItemDisplayed(foodList, index);
+          },
+          itemCount: foodList.length,
+        );
+      },
+      listener: (BuildContext context, foodList) {},
+    );
+  }
+
+  Card designItemDisplayed(List<Food> foodList, int index) {
+    Food food = foodList[index];
+    return Card(
+      child: ListTile(
+        contentPadding: EdgeInsets.all(16),
+        title: Text(food.name, style: TextStyle(fontSize: 26)),
+        subtitle: Text(
+          "id: ${food.id}",
+          style: TextStyle(fontSize: 20),
+        ),
+        onTap: () => showFoodDialog(context, food, index),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    print("Building entire food list scaffold");
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Persistent storage"),
-      ),
       body: Container(
         padding: EdgeInsets.all(8),
         color: Colors.grey,
-
-        ///a tout moment ou qqchose change dans le foodbloc, le bloc consumer
-        /// sera updated
-        child: BlocConsumer<FoodBloc, List<Food>>(
-          builder: (context, foodList) {
-            return ListView.builder(
-              itemBuilder: (BuildContext context, int index) {
-                print("foodList: $foodList");
-
-                Food food = foodList[index];
-                return Card(
-                  child: ListTile(
-                    contentPadding: EdgeInsets.all(16),
-                    title: Text(food.name, style: TextStyle(fontSize: 26)),
-                    subtitle: Text(
-                      "id: ${food.id}",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    onTap: () => showFoodDialog(context, food, index),
-                  ),
-                );
-              },
-              itemCount: foodList.length,
-            );
-          },
-          listener: (BuildContext context, foodList) {},
-        ),
+        //quand change dans le foodbloc, le bloc consumer sera updated
+        child: getBlocConsummer(),
       ),
     );
   }
 }
-
-
-/***
- *  BlocConsumer is analogous to a nested BlocListener and BlocBuilder
- *  BlocConsumer should only be used when it is necessary to both rebuild UI and
- *  execute other reactions to state changes in the bloc.
- *  -required BlocWidgetBuilder and BlocWidgetListener
- */
