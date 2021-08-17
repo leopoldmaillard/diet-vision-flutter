769148bd21f864a3b4c68d273530b5464bbb93c1
Michael <misoko.hofmann@gmail.com>
2021-08-17 16:56:44 +0200

    #5 retrive basic struff on main branch

--- lib/fhome.dart
+++ lib/fhome.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:transfer_learning_fruit_veggies/services/login.dart';
 
 class FHomeScreen extends StatelessWidget {
+  final auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Container();
